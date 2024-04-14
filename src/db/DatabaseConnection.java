22cb27059345eec0ad1e17694edb805db5a8b505
Raúl Escamilla <raul.escamilla@asesoriait.com>
2024-04-14 02:15:30 -0600

    Database Connection Stablished

--- src/db/DatabaseConnection.java
+++ src/db/DatabaseConnection.java
@@ -1,5 +1,39 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+ 
+    private static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";  
+    private static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=FinanFX;encrypt=true;trustServerCertificate=true";;
+    private static final String DB_USERNAME = "UserDSIW1_0124";
+    private static final String DB_PASSWORD = "User13*";
     
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Error al cargar el controlador JDBC", e);
+        }
+
+        return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    }
+
+    public static void closeConnection(Connection connection) throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static boolean testConnection() throws SQLException {
+        Connection connection = getConnection();
+        if (connection != null) {
+            closeConnection(connection);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/finanfx/main.java
+++ src/finanfx/main.java
@@ -1,10 +1,19 @@
 package finanfx;
 
+import db.DatabaseConnection;
+import java.sql.SQLException;
 
 public class main {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            if (DatabaseConnection.testConnection()) {
+                System.out.println("Connection to database successful!");
+            } else {
+                System.err.println("Failed to connect to database!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error while testing connection: " + e.getMessage());
+        }
     }
-    
 }
