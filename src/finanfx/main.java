580674a105fd468f5d9cf8c4ccfb010b0d061df0
13Anderson13 <167251508+13Anderson13@users.noreply.github.com>
2024-04-17 11:48:26 -0600

    Prueba de conexión

--- src/finanfx/main.java
+++ src/finanfx/main.java
@@ -1,8 +1,18 @@
 package finanfx;
+import java.sql.SQLException;
+import finanfx.data.DatabaseConnection;
 
 public class main {
 
     public static void main(String[] args) {
-       
+       try {
+            if (DatabaseConnection.testConnection()) {
+                System.out.println("Connection to database successful!");
+            } else {
+                System.err.println("Failed to connect to database!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error while testing connection: " + e.getMessage());
+        }
     }
 }
