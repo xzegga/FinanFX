40845e30e20984609d450545b3456f3812f4743d
13Anderson13 <167251508+13Anderson13@users.noreply.github.com>
2024-04-21 09:09:48 -0600

    Login

--- src/finanfx/main.java
+++ src/finanfx/main.java
@@ -1,8 +1,21 @@
 package finanfx;
+import Formularios.frmLogin;
+//import java.sql.SQLException;
+//import finanfx.data.DatabaseConnection;
 
 public class main {
 
     public static void main(String[] args) {
-       
+       /*try {
+            if (DatabaseConnection.testConnection()) {
+                System.out.println("Connection to database successful!");
+            } else {
+                System.err.println("Failed to connect to database!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error while testing connection: " + e.getMessage());
+        }*/
+       frmLogin login = new frmLogin();
+       login.setVisible(true);
     }
 }
